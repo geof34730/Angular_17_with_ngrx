63ff931d34b2afb060af58a179f679fa76ba9f1a
geoffreypetain <geoffrey.petain@gmail.com>
2024-06-04 11:15:21 +0200

    add devetools and create module state

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, isDevMode} from '@angular/core';
 import {provideRouter, withComponentInputBinding, withDebugTracing} from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,24 +7,25 @@ import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryDataService} from "./in-memory-data.service";
 import {HttpClientModule, provideHttpClient, withFetch} from "@angular/common/http";
 import pokemonData from "./datas/pokemon.data";
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
- /*   provideRouter(routes,withComponentInputBinding(),withDebugTracing()),
-    provideClientHydration(),
-    //importProvidersFrom(HttpClientModule),
-    //HttpClientModule,
-    //provideHttpClient(withFetch()),
-    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(ApiInMemoryService,{dataEncapsulation:false})),
-*/
+    /*   provideRouter(routes,withComponentInputBinding(),withDebugTracing()),
+       provideClientHydration(),
+       //importProvidersFrom(HttpClientModule),
+       //HttpClientModule,
+       //provideHttpClient(withFetch()),
+       importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(ApiInMemoryService,{dataEncapsulation:false})),
+   */
     provideRouter(routes),
     provideClientHydration(),
     importProvidersFrom(HttpClientModule),
     //HttpClientModule,
     //provideHttpClient(withFetch()),
-    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation:false}))
-
-  ],
+    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false })),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+],
 
 };
